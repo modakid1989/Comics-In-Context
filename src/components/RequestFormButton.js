b9d05a122ac9aa05d8171adc40d1489b66c9da77
Jonphia Hart <soldiergurl4life@gmail.com>
2023-03-23 18:08:53 -0400

    finshed assignment but it's not working

---
+++ src/components/RequestFormButton.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import StylesContext from "../components/StylesContext";
+
+
+export default function RequestFormButton() {
+  const buttonStyling = React.useContext(StylesContext)
+
+  return (
+    <div>
+      <button style={buttonStyling}>
+        Click Here!
+      </button>
+      <input style={buttonStyling} type="submit" value="Submit" />
+
+    </div>
+  );
+}
